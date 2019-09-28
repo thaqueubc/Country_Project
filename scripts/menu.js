2a2728bef739a911b84dddfda443fc5bf7fc1324
Tasnuva Haque <reya.ruet@gmail.com>
2019-09-28 11:06:00 -0700

    nav updated, banner image added

---
+++ scripts/menu.js
@@ -0,0 +1,8 @@
+const body = document.body;
+const btn = document.getElementById('.btn');
+
+btn.addEventListener('click', _ => body.classList.toggle('show'));
+
+// Prevents the focus state from activating on mousedown but
+// keeps the focus state for keyboard users
+btn.addEventListener('mousedown', (e) => { e.preventDefault() });
