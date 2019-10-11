ce290ff40d45747eae52d3384adb139c40bbda96
Tasnuva Haque <A01174842@ad.bcit.ca>
2019-10-11 15:41:15 -0700

    sticky header added

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,10 @@
 const body = document.body;
 const btn = document.getElementById('nav-button');
+//const submenu = document.getElementById('sub-menu');
 
 btn.addEventListener('click', _ => body.classList.toggle('show'));
+
+//submenu.addEventListener('click', _ => body.classList.toggle('show-sub-menu'));
 
 // Prevents the focus state from activating on mousedown but
 // keeps the focus state for keyboard users
